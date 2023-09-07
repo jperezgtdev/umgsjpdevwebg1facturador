bc77f43263f13cf06e0f7fdfe862da51d42c3b1a
Jerber <jcintor@miumg.edu.gt>
2023-09-06 23:32:58 -0600

    se agrega los templates

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -117,6 +117,15 @@
               </li>
             </ul>
           </li>
+
+          <li class="nav-item">
+                <a href="<?= site_url('VentaController/index')?>" class="nav-link">
+                <img src="<?= base_url('imagenes/venta.png') ?>" style="width: 20px; height: 20px;">
+                  <p>Venta</p>
+                </a>
+              </li>
+            </ul>
+          </li>
           
           <li class="nav-header">USUARIO</li>
 
